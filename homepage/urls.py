f210567a916a138539e1d2709e0da199377d0dbf
Alejandro Morales <alejandro.morales04@gmail.com>
2022-09-30 21:59:55 -0500

    commiting all changes

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from homepage import views
+from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("homepage/name", views.hello_there, name="hello_there"),
+    path("", views.index, name="index"),
+    path("homepage/login.html", views.login, name="login"),
+    path("homepage/<str:name>", views.hello_there, name="User_Redirect"),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils.timezone import datetime
 
 # Create the formatting for the home page here.
-def home(request):
+def index(request):
     return HttpResponse("Hello placeholder text")
 
-# Placeholder Template 
+# Set up the formatting for the welcome page.
+def login(request):
+    return render(request, 'homepage/login.html')
+
+# URL routing to a name 
 def hello_there(request, name):
     return render(
         request,
