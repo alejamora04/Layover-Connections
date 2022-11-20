9e09cf123d9f541276448c771b445fbcecd6f0ed
Alejandro Morales <alejandro.morales04@gmail.com>
2022-11-20 09:13:47 -0600

    Changed name of app from homepage to layover connections

--- Layover_Connections/settings.py
+++ Layover_Connections/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'homepage',
+    'layoverconnections',
+    'django_rename_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- homepage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from homepage import views
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("homepage/login.html", views.login, name="login"),
-    path("homepage/<str:name>", views.user_profile, name="profile"),
-]
