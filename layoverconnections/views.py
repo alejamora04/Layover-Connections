6bd3dd5c2af2ae714718e3dc0a4abf337dcdbf75
Alejandro Morales <alejandro.morales04@gmail.com>
2022-11-25 20:28:03 -0600

    Incorporated Anchor Redirects between login and regsiter

--- layoverconnections/views.py
+++ layoverconnections/views.py
@@ -17,7 +17,7 @@ def register_request(request):
 			user = form.save()
 			login(request, user)
 			messages.success(request, "Registration successful." )
-			return redirect("layoverconnections/login.html")
+			return redirect("layoverconnections:login.html")
 		messages.error(request, "Unsuccessful registration. Invalid information.")
 	form = NewUserForm()
 	return render (request=request, template_name="layoverconnections/register.html", context={"register_form":form})
